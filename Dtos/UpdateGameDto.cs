4ac85c53dae3cb7655b653ba2fbd4aac03110fbb
tomaszsliwakowski <tomek08082002@gmail.com>
2024-06-16 12:46:41 +0200

    new file:   .gitignore 	new file:   Data/GameStoreContext.cs 	modified:   Dtos/CreateGameDto.cs 	modified:   Dtos/UpdateGameDto.cs 	new file:   Endpoints/GamesEndpoints.cs 	new file:   Entities/Game.cs 	new file:   Entities/Genre.cs 	modified:   GameStore.Api.csproj 	modified:   Program.cs 	modified:   appsettings.json 	modified:   bin/Debug/net8.0/GameStore.Api.deps.json 	modified:   bin/Debug/net8.0/GameStore.Api.dll 	modified:   bin/Debug/net8.0/GameStore.Api.exe 	modified:   bin/Debug/net8.0/GameStore.Api.pdb 	modified:   bin/Debug/net8.0/GameStore.Api.runtimeconfig.json 	modified:   bin/Debug/net8.0/appsettings.json 	modified:   obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs 	modified:   obj/Debug/net8.0/GameStore.Api.AssemblyInfoInputs.cache 	modified:   obj/Debug/net8.0/GameStore.Api.assets.cache 	modified:   obj/Debug/net8.0/GameStore.Api.csproj.CoreCompileInputs.cache 	modified:   obj/Debug/net8.0/GameStore.Api.csproj.FileListAbsolute.txt 	modified:   obj/Debug/net8.0/GameStore.Api.dll 	modified:   obj/Debug/net8.0/GameStore.Api.genruntimeconfig.cache 	modified:   obj/Debug/net8.0/GameStore.Api.pdb 	modified:   obj/Debug/net8.0/apphost.exe 	modified:   obj/Debug/net8.0/ref/GameStore.Api.dll 	modified:   obj/Debug/net8.0/refint/GameStore.Api.dll 	modified:   obj/GameStore.Api.csproj.nuget.dgspec.json 	modified:   obj/GameStore.Api.csproj.nuget.g.props 	modified:   obj/GameStore.Api.csproj.nuget.g.targets 	modified:   obj/project.assets.json 	modified:   obj/project.nuget.cache

--- Dtos/UpdateGameDto.cs
+++ Dtos/UpdateGameDto.cs
@@ -1,7 +1,9 @@
-﻿namespace GameStore.Api.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
 
-public record class UpdateGameDto(  
-    string Name,
-    string Genre,
-    decimal Price,
-    DateOnly ReleaseDate);
+namespace GameStore.Api.Dtos;
+
+public record class UpdateGameDto(
+      [Required][StringLength(50)] string Name,
+      [Required][StringLength(20)] string Genre,
+      [Range(1, 100)] decimal Price,
+       DateOnly ReleaseDate);
